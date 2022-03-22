2b8641a4a3bfc392ac1703c4edc87d79e91bf909
Malik Kennedy <mksybr@gmail.com>
2022-03-22 14:59:04 -0400

    ci: local file?

--- .circleci/install_quicklisp.sh
+++ .circleci/install_quicklisp.sh
@@ -3,5 +3,5 @@
 curl -O https://beta.quicklisp.org/quicklisp.lisp
 curl -O https://beta.quicklisp.org/quicklisp.lisp.asc
 gpg --verify quicklisp.lisp.asc quicklisp.lisp
-sbcl --load quicklisp.lisp --eval "(quicklisp-quickstart:install)"
+sbcl --load ./quicklisp.lisp --eval "(quicklisp-quickstart:install)"
 
