ab3461353b914f3f055b0c94ca9900dc93f94f9c
Malik Kennedy <mksybr@gmail.com>
2022-03-22 15:04:50 -0400

    bugfix: will I ever learn, its always from project root

--- .circleci/install_quicklisp.sh
+++ .circleci/install_quicklisp.sh
@@ -2,6 +2,7 @@
 
 curl -O https://beta.quicklisp.org/quicklisp.lisp
 curl -O https://beta.quicklisp.org/quicklisp.lisp.asc
-gpg --verify quicklisp.lisp.asc quicklisp.lisp
-sbcl --disable-debugger --eval "(load \"quicklisp.lisp\")"  --eval "(quicklisp-quickstart:install)"      --eval "(ql-util:without-prompting (ql:add-to-init-file))"   --eval "(ql-util:without-prompting (ql:update-all-dists))" --eval "(sb-ext:exit)"
+gpg --verify ./.circleci/quicklisp.lisp.asc quicklisp.lisp
+ls; pwd; echo $PATH
+sbcl --disable-debugger --eval "(load \"./.circleci/quicklisp.lisp\")"  --eval "(quicklisp-quickstart:install)"      --eval "(ql-util:without-prompting (ql:add-to-init-file))"   --eval "(ql-util:without-prompting (ql:update-all-dists))" --eval "(sb-ext:exit)"
 
