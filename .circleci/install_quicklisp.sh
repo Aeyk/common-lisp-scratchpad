c182347d64ea38c10b98c4eda3a77a613dfbab86
Malik Kennedy <mksybr@gmail.com>
2022-03-22 15:03:22 -0400

    chore: install quicklisp script for ci

--- .circleci/install_quicklisp.sh
+++ .circleci/install_quicklisp.sh
@@ -3,5 +3,5 @@
 curl -O https://beta.quicklisp.org/quicklisp.lisp
 curl -O https://beta.quicklisp.org/quicklisp.lisp.asc
 gpg --verify quicklisp.lisp.asc quicklisp.lisp
-sbcl --load ./quicklisp.lisp --eval "(quicklisp-quickstart:install)"
+sbcl --disable-debugger --eval "(load \"quicklisp.lisp\")"  --eval "(quicklisp-quickstart:install)"      --eval "(ql-util:without-prompting (ql:add-to-init-file))"   --eval "(ql-util:without-prompting (ql:update-all-dists))" --eval "(sb-ext:exit)"
 
